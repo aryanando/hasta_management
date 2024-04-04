708ca9606a3026a0f62f749d7af408d2f61aa92b
Aryanando Mac <aryanando232@gmail.com>
2024-04-04 10:56:42 +0700

    Rules To Token

--- app/Http/Controllers/AbsensiTokenController.php
+++ app/Http/Controllers/AbsensiTokenController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Http;
 class AbsensiTokenController extends Controller
 {
     public function index() {
+        $data['user_data'] = session('user_data');
+        if ($data['user_data']->id != 1) {
+            return redirect('/karu');
+        }
         return view('absensi_token');
     }
 }
