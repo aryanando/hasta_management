6dd955141c09f3665a59bb6233008112309087ea
DDStrw <satriodandung00@gmail.com>
2024-07-16 11:39:44 +0700

    judul dan auth wasin

--- resources/views/wasin/layout/layout.blade.php
+++ resources/views/wasin/layout/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-
+    <title>{{ $page_info['title'] }}</title>
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
