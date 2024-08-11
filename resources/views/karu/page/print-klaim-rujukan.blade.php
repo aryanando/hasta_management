694126f7fa81992d9b329ff765351c2558c5b309
DDStrw <satriodandung00@gmail.com>
2024-08-11 15:04:39 +0700

    cetak

--- resources/views/karu/page/print-klaim-rujukan.blade.php
+++ resources/views/karu/page/print-klaim-rujukan.blade.php
@@ -1,12 +1,103 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Print</title>
-</head>
-<body>
-    @dd($rujukan)
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+        <title>Print</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            @media print {
+
+                body,
+                .container {
+                    width: 210mm;
+                    /* A4 width */
+                    height: 297mm;
+                    /* A4 height */
+                    margin: 0 auto;
+                }
+
+                .container {
+                    max-width: none !important;
+                }
+            }
+        </style>
+    </head>
+
+    <body id="allprint">
+        <div class="container">
+            <div class="bg-white rounded mt-3 shadow p-4">
+                <div class="d-flex align-items-center mb-3">
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" class="me-3"
+                        style="height: 150px; width: 150px;">
+
+                    <div class="flex-grow-1 text-center">
+                        <h1 class="mb-0">Fee Rujuk Masuk</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>Nama Pasien</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">{{ $rujukan->nama_pasien }}</div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>No. Reg Periksa</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">{{ $rujukan->no_reg_periksa }}</div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>Nama Perujuk</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">{{ $rujukan->nama_perujuk }}</div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>No. HP</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">{{ $rujukan->no_hp }}</div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>Keterangan</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">{{ $rujukan->keterangan }}</div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>Petugas Kasir</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">{{ $rujukan->petugas_kasir->name ?? 'N/A' }}</div>
+                        </div>
+                        @if (!is_null($rujukan->petugas_pendaftaran))
+                            <div class="row mb-2">
+                                <div class="col-3"><strong>Petugas Pendaftaran</strong></div>
+                                <div class="col-1 text-end">:</div>
+                                <div class="col-8">{{ $rujukan->petugas_pendaftaran->name }}</div>
+                            </div>
+                        @endif
+
+                        @if (!is_null($rujukan->perujuk_blu))
+                            <div class="row mb-2">
+                                <div class="col-3"><strong>Perujuk BLU</strong></div>
+                                <div class="col-1 text-end">:</div>
+                                <div class="col-8">{{ $rujukan->perujuk_blu->name ?? '-' }}</div>
+                            </div>
+                        @endif
+                        <div class="row mb-2">
+                            <div class="col-3"><strong>Biaya</strong></div>
+                            <div class="col-1 text-end">:</div>
+                            <div class="col-8">Rp. {{ number_format($rujukan->biaya, 0, ',', '.') }}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- <button id="download" class="btn btn-primary mt-3">Download PDF</button> --}}
+        </div>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+    </html>
